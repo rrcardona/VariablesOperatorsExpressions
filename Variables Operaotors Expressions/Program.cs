d5580326f43f1d76218bfe79ab3128c1dd97db62
rrcardona <cardonas05@yahoo.com>
2017-08-15 19:35:02 -0400

    first commit

---
+++ Variables Operaotors Expressions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Operators_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
