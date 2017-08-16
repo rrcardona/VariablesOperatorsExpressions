f7128d829147095c15a45fb3740b13c59733b27b
rrcardona <cardonas05@yahoo.com>
2017-08-15 20:50:37 -0400

    Created more variable types

--- Variables Operaotors Expressions/Program.cs
+++ Variables Operaotors Expressions/Program.cs
@@ -10,6 +10,39 @@ namespace Variables_Operators_Expressions
     {
         static void Main(string[] args)
         {
+            string name = "Ricardo";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+            string Name = "Ricardo";
+
+            string birthMonth = "February";
+            //Console.WriteLine(birthMonth);
+            int birthMonthNumber = 2;
+            //integers
+            int age = 65;
+            int population = 100000;
+            //Floating Type Variables
+            float number = 2.1644003304897294383961032f;
+            double someNumber = 2.1644003304897294383961032d;
+            decimal partialNumber = 2.1644003304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+            //Boolean 
+            bool isPresent = true;
+            bool seatTaken = false;
+            //Character
+            char lastLetter = 'j';
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge < samAge);
+
+
         }
     }
 }
